506024fd24f268fc4a66d7c94be1e0c4bc437bfb
Colin <colingraf0@gmail.com>
2026-09-23 20:37:39 +0200

    Prepare website downloads for v1.5.0

--- website/downloads.js
+++ website/downloads.js
@@ -1,6 +1,6 @@
 const repository = "colin515/uek-notizen";
-const preferredRelease = "v1.4.0";
-const safeFallbackRelease = "v1.3.0";
+const preferredRelease = "v1.5.0";
+const safeFallbackRelease = "v1.4.0";
 
 function applyRelease(release) {
   const assets = release.assets || [];
